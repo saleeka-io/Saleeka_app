780e143b8ccc78b25664e43e93cc18f3b7ec94e6
sohal786 <91308200+sohal786@users.noreply.github.com>
2023-08-26 10:23:59 -0700

    initial commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Router from 'react'
+
+function Navbar() {
+  return (
+   <>
+        <nav className="navbar">
+            <div className="navbar-container">
+
+                </div>
+                
+        </nav>
+        
+    </>
+  )
+}
+
+export default Navbar
